994cdf269bfef1b39e2d9b7bc44ed8587a8b5dce
Sebastian Schöneich <Sebastian Schneich>
2024-07-04 22:38:15 +0200

    Added saving to File and Reading From it

--- mainE.java
+++ mainE.java
@@ -2,7 +2,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 import javax.swing.*;
 
@@ -13,7 +17,6 @@ public class mainE {
 	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		JFrame Frame= new JFrame();
 		
 		JTextField TextField = new JTextField();
@@ -27,6 +30,51 @@ public class mainE {
 				List<String> selected = list.getSelectedValuesList();
 			}
 		});
+
+		try {
+			File file = new File("saved_list.txt");
+			if (file.createNewFile()) {
+				System.out.println("File created: " + file.getName());
+			} else {
+				System.out.println("File already exists");
+			}
+
+			Scanner scanner = new Scanner(file);
+			while(scanner.hasNextLine()) {
+				String data = scanner.nextLine();
+				internalList.addElement(data);
+			}
+			scanner.close();
+
+		} catch(IOException e) {
+			System.out.println("An IO Exception occured");
+			e.printStackTrace();
+		}
+
+		
+
+			Frame.addWindowListener(new java.awt.event.WindowAdapter() {
+				@Override
+				public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+					try {
+						FileWriter fileWriter = new FileWriter("saved_list.txt");
+						if (JOptionPane.showConfirmDialog(Frame, "Willst du die Liste speichern?",
+														"Liste speichern?", JOptionPane.YES_NO_OPTION,
+														JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+							String elementTempString = internalList.toString();
+							String[] elementArray = elementTempString.split("(\\[|\\]|\\,\\s)");
+							for (String element : elementArray) {
+								fileWriter.write(element + "\n");
+							}
+						}
+						fileWriter.close();
+					} catch(IOException e) {
+						System.out.println("An IO Exception occured");
+						e.printStackTrace();
+					}
+				}
+			});
+
 
 		JButton addButton = new JButton("Hinzu");
 		addButton.addActionListener((ActionListener) new ActionListener() {
