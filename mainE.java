600f7da1a250a3fafed2f7013ec2357aa9bc7e72
Sebastian Schöneich <Sebastian Schneich>
2024-07-05 09:37:54 +0200

    Added Comments

--- mainE.java
+++ mainE.java
@@ -13,24 +13,36 @@ import javax.swing.*;
 import static javax.swing.JOptionPane.showMessageDialog;
 
 
+// Aufgabenaufteilung:
+//
+// Paul Weibbrecht:
+// * Erstellung des Grundgerüstes der Software
+// * Grafische Darstellung
+// * grundsätzliches Event-Handling
+//
+// Sebastian Schöneich:
+// * Softwarelogik
+// * Datei-Handling
+// * Finalisierung und Tests
+
 public class mainE {
-	
 
 	public static void main(String[] args) {
+		// initialising swing elements
 		JFrame Frame= new JFrame();
-		
 		JTextField TextField = new JTextField();
-
 		DefaultListModel internalList = new DefaultListModel<>();
 		JList list = new JList();
 		list.setModel(internalList);
 
+		// adding EventListener to JList-Object
 		list.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				List<String> selected = list.getSelectedValuesList();
 			}
 		});
 
+		// reading saved_list.txt and adding elements to list at startup
 		try {
 			File file = new File("saved_list.txt");
 			if (file.createNewFile()) {
@@ -51,31 +63,30 @@ public class mainE {
 			e.printStackTrace();
 		}
 
-		
-
-			Frame.addWindowListener(new java.awt.event.WindowAdapter() {
-				@Override
-				public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-					try {
-						FileWriter fileWriter = new FileWriter("saved_list.txt");
-						if (JOptionPane.showConfirmDialog(Frame, "Willst du die Liste speichern?",
-														"Liste speichern?", JOptionPane.YES_NO_OPTION,
-														JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
-							String elementTempString = internalList.toString();
-							String[] elementArray = elementTempString.split("(\\[|\\]|\\,\\s)");
-							for (String element : elementArray) {
-								fileWriter.write(element + "\n");
-							}
+		// on closing, asks the user whether they want to save the list to the saved_list.txt file
+		Frame.addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+				try {
+					FileWriter fileWriter = new FileWriter("saved_list.txt");
+					if (JOptionPane.showConfirmDialog(Frame, "Willst du die Liste speichern?",
+													"Liste speichern?", JOptionPane.YES_NO_OPTION,
+													JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+						String elementTempString = internalList.toString();
+						String[] elementArray = elementTempString.split("(\\[|\\]|\\,\\s)");
+						for (String element : elementArray) {
+							fileWriter.write(element + "\n");
 						}
-						fileWriter.close();
-					} catch(IOException e) {
-						System.out.println("An IO Exception occured");
-						e.printStackTrace();
 					}
+					fileWriter.close();
+				} catch(IOException e) {
+					System.out.println("An IO Exception occured");
+					e.printStackTrace();
 				}
-			});
+			}
+		});
 
-
+		// adds add-button
 		JButton addButton = new JButton("Hinzu");
 		addButton.addActionListener((ActionListener) new ActionListener() {
 		    @Override
@@ -91,6 +102,7 @@ public class mainE {
 		    }
 		});
 		
+		// adds mark-button
 		JButton markButton = new JButton("Erledigt");
 		markButton.addActionListener((ActionListener) new ActionListener() {
 		    @Override
@@ -105,6 +117,7 @@ public class mainE {
 		    }
 		});
 		
+		// adds remove-button
 		JButton removeButton = new JButton("Liste Löschen");
 		removeButton.addActionListener((ActionListener) new ActionListener() {
 		    @Override
@@ -114,12 +127,14 @@ public class mainE {
 		    }
 		});
 		
+		// set the dimensions of the window
 		TextField.setBounds(10, 10, 380, 20);
 		list.setBounds(10, 40, 380, 380);
 		addButton.setBounds(290, 430, 100, 25);
 		markButton.setBounds(190, 430, 80, 25);
 		removeButton.setBounds(10, 430, 120, 25);
 		
+		// add all objects to the frame and sets some options
 		Frame.add(TextField);
 		Frame.add(list);
 		Frame.add(addButton);
